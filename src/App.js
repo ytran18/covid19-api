03afe009fd9a973b3fd1a02032c79fde44730a0d
jineu <a11718trannhuy@gmail.com>
2022-08-17 11:36:51 +0700

    style

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { Select } from 'grommet'
 
-import DrawChart from './DrawChart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVirusCovid } from '@fortawesome/free-solid-svg-icons'
+
+import CityInfo from './CityInfo';
 
 function App() {
 
     const url =`https://api.apify.com/v2/key-value-stores/EaCBL1JNntjR3EakU/records/LATEST?disableRedirect=true`
 
     const names = ['Hà Nội','TP. Hồ Chí Minh','Hải Phòng','Nghệ An','Bắc Giang','Vĩnh Phúc','Hải Dương','Quảng Ninh','Bắc Ninh','Thái Nguyên','Phú Thọ','Bình Dương','Nam Định','Thái Bình','Hưng Yên','Hòa Bình','Lào Cai','Đắk Lắk','Thanh Hóa','Lạng Sơn','Sơn La','Yên Bái','Cà Mau','Tuyên Quang','Tây Ninh','Bình Định','Quảng Bình','Hà Giang','Khánh Hòa','Bình Phước','Bà Rịa & Vũng Tàu','Đồng Nai','Đà Nẵng','Vĩnh Long','Ninh Bình','Bến Tre','Cao Bằng','Lâm Đồng','Điện Biên','Hà Nam','Quảng Trị','Cần Thơ','Bắc Kạn','Trà Vinh','Lai Châu','Đắk Nông','Gia Lai','Bình Thuận','Hà Tĩnh','Đồng Tháp','Quảng Nam','Long An','Quảng Ngãi','Thừa Thiên Huế','Bạc Liêu','Phú Yên','An Giang','Kiên Giang','Tiền Giang','Sóc Trăng','Kon Tum','Hậu Giang','Ninh Thuận']
-    
     
     const [data, setData] = useState({})
     const [value, setValue] = useState('')
@@ -25,13 +27,6 @@ function App() {
         
     },[url])
     
-    // console.log(value);
-    // const cityName = data.locations.map((e) => e.name)
-    // console.log(cityName);
-
-    // console.log( typeof JSON.parse(data)['locations'][0]['name'] );
-    
-    
     return (
     <div className="app">
         <div className='container'>
@@ -41,49 +36,55 @@ function App() {
 
             <div className='generalInfo'>
                 <div className='infected'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Infected</p>
                     {`${data.infected} people`}
                 </div>
                 <div className='recovered'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Recovered</p>
                     {`${data.recovered} people`}
                 </div>
                 <div className='died'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Died</p>
                     {`${data.died} people`}
                 </div>
             </div>
 
-            <br />  <br />  <br />
+            <br />  
 
             <div className='todayInfo'>
                 <div className='infectedToday'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Infected Today</p>
                     {`${data.infectedToday} people`}
                 </div>
                 <div className='recoveredToday'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Recovered Today</p>
                     {`${data.recoveredToday} people`}
                 </div>
                 <div className='diedToday'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
                     <p>Died Today</p>
                     {`${data.diedToday} people`}
                 </div>
             </div>
 
-            <br />  <br />  <br />
 
             <div className='cityInfo'>
                 <div className='search'>
+                    <h3 className='txtPickCity'>Pick city</h3>
                     <Select 
                         value={value}
                         onChange={({ option }) => setValue(option)}
                         options={names}
                     />
-                    
+                    <br /> <br />
                 </div>
                 <div className='details'>
-                    <DrawChart location={value}/>
+                    <CityInfo location={value}/>
                 </div>
             </div>
         </div>
---
+++ src/CityInfo.js
@@ -0,0 +1,130 @@
+import './CityInfo.css'
+import axios from 'axios'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVirusCovid } from '@fortawesome/free-solid-svg-icons'
+
+// import 
+// {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// } from 'chart.js'
+
+import { useEffect, useState } from 'react'
+
+// import { Bar } from 'react-chartjs-2'
+
+// ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// )
+
+
+
+
+function CityInfo ({ location })
+{
+
+    const [info, setInfo] = useState({})
+
+    const url =`https://api.apify.com/v2/key-value-stores/EaCBL1JNntjR3EakU/records/LATEST?disableRedirect=true`
+
+    useEffect(() =>
+    {
+        axios.get(url).then((res) =>
+        {
+            setInfo(res.data)
+        }) 
+    },[url]) 
+
+
+    
+    let locationInfo = info.locations
+
+    let numberCase = 0
+    let numberDeath = 0
+    let numberRecovered = 0
+    let numberCase1 = 0
+    let numberDeath1 = 0
+    let numberRecovered1 = 0
+   
+    if( typeof locationInfo !== 'undefined')
+    {
+        let index = 0
+        for (index; index < 63; index++) {
+            if( locationInfo[index].name == location)
+            {
+                numberCase = locationInfo[index].cases
+                numberDeath = locationInfo[index].death
+                numberRecovered = locationInfo[index].recovered
+            }
+            
+        }
+        numberCase1 = numberCase
+        numberDeath1 = numberDeath
+        numberRecovered1 = numberRecovered
+    }
+
+    // const options = {
+    //     responsive: true,
+    //     plugins: {
+    //         legend: {
+    //             position: 'top'
+    //         },
+    //     title: {
+    //         display: true,
+    //         text: `Covid 19 in ${location}`,
+    //     },
+    //   },
+    // }
+
+    // const labels = ['data']
+
+    // const data = {
+    //     labels,
+    //     datasets: [
+    //         {
+    //           label: 'Cases',
+    //           data: `${numberCase1}`,
+    //           backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    //         },
+    //         {
+    //           label: 'Death',
+    //           data: `${numberDeath1}`,
+    //           backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    //         },
+    //       ],
+    // }
+
+    return(
+        <div className='todayInfo'>
+            {/* <Bar options={options} data={data} /> */}
+                <div className='if'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
+                    <p>Infected</p>
+                    {`${numberCase1} people`}
+                </div>
+                <div className='re'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
+                    <p>Recovered</p>
+                    {`${numberRecovered1} people`}
+                </div>
+                <div className='di'>
+                    <FontAwesomeIcon icon={faVirusCovid} size={'4x'}/>
+                    <p>Died</p>
+                    {`${numberDeath1} people`}
+                </div>
+            </div>
+    )
+}
+
+export default CityInfo
